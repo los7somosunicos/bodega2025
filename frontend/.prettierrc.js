3718bc6ca5a0ca2deeed72041809065a9e696707
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-01-20 15:26:56 -0600

    update 2

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "none",
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  printWidth: 105,
+  endOfLine: "auto",
+  singleQuote: false,
+  useTabs: false
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div>
-      hola mundo
-    </div>
-  );
+  return <div>hola mundo</div>;
 }
