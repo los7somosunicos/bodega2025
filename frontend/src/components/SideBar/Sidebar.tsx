d3561fd7d0b768e44993ae285c633abc0c9cb2d9
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-02-03 15:57:10 -0600

    edit global css

--- frontend/src/components/SideBar/Sidebar.tsx
+++ frontend/src/components/SideBar/Sidebar.tsx
@@ -5,13 +5,13 @@ const Sidebar = (): React.ReactElement => {
   return (
     <aside
       className={cx(
-        `h-[100vh] w-[300px] bg-blue-950 text-white flex flex-col items-center shadow-2xl transition ease-in-out duration-300`,
+        `h-[100vh] w-[300px] bg-blue-950 text-white text-center shadow-2xl transition ease-in-out duration-300`,
         {
           "opacity-0 w-0": false
         }
       )}
     >
-      <div className="font-bold mt-10">Control de Ingresos</div>
+      <p className="font-bold mt-10">Control de Ingresos</p>
     </aside>
   );
 };
