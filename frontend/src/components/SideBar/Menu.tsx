b56a017df2a53a5fab2e1638620d4fcfa5868bc5
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-02-13 15:52:50 -0600

    update sidebar menu

--- frontend/src/components/SideBar/Menu.tsx
+++ frontend/src/components/SideBar/Menu.tsx
@@ -7,7 +7,7 @@ function Menu({ menu, title }: MenuProps): React.ReactElement {
   return (
     <div className="w-full h-full flex flex-col">
       <p className="font-bold mt-10">{title}</p>
-      <ul className="flex flex-col items-center mt-10">
+      <ul className="flex flex-col items-center mt-30 gap-3">
         {menu.map((item: MenuType, index: number) => (
           <MenuItem title={item.title} path={item.path} key={index} />
         ))}
--- frontend/src/components/SideBar/MenuItem.tsx
+++ frontend/src/components/SideBar/MenuItem.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import Link from "next/link";
 import cx from "classnames";
 import { MenuType } from "@/models/menu.type";
+import { usePathname } from "next/navigation";
+
 
 const MenuItem = ({ title, path }: MenuType): React.ReactElement => {
+    const currentPath = usePathname()
   return (
-    <li className={cx(`w-full h-10 border-0.5 border-gray-600 inline-block align-middle`)}>
+    <li 
+      className={cx(
+        'w-[80%] h-10 rounded flex items-center justify-center hover:text-black hover:bg-white hover:font-bold',
+      {
+        'bg-white text-black font-bold':currentPath === path
+      }
+      )}>
       <Link href={path}>{title}</Link>
     </li>
   );
