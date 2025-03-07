2f1fcca43cb39789849fe492247912810cc31503
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2025-03-07 15:57:40 -0600

    add auth and loand endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config'
 import { validate } from './config/env.validation';
 import { AssetModule } from './asset/asset.module';
 import { CategoryModule } from './category/category.module';
+import { LoanModule } from './loan/loan.module';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 
 
@@ -14,7 +16,9 @@ import configuration from './config/configuration';
       load:[configuration],
     }),
     AssetModule,
-    CategoryModule
+    CategoryModule,
+    LoanModule,
+    AuthModule
   ],
   controllers: [],
   providers: [],
